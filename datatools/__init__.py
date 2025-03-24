e02c8d07ccc98c6508fd2ea75cc4105333ce6e3c
Devoane <162355799+Devoane@users.noreply.github.com>
2025-03-24 17:13:07 +0100

    add

---
+++ datatools/__init__.py
@@ -0,0 +1,12 @@
+"""
+DataTools - a collection of tools for data processing.
+
+Contains functions for text processing, data analysis, and other useful utilities.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Devoan'
+
+from . import text_processing
+from . import stats_analysis
+from . import file_utils
